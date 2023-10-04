a2f16a30d0fea33178924cf816fe65359c204de1
Volodymyr <vovkul@gmail.com>
2023-10-04 23:39:18 +0200

    add task 1 HW8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+gallery.style.listStyle = 'none';
+// console.dir(gallery);
+
+gallery.insertAdjacentHTML('afterbegin', createMarkUp(galleryItems));
+// gallery.addEventListener("click", handlerClick);
+
+function createMarkUp(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+   <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+       </li>`
+    )
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
