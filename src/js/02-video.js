2737e5fd8e67dd0ae4f162364f2e83cfdfa8136f
Volodymyr <vovkul@gmail.com>
2023-10-09 10:13:00 +0200

    add task 2 (doesnt work)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const KEY_STORAGE = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+iframe.style.margin = '20px auto';
+iframe.style.display = 'flex';
+
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(updatingTimeLocalStorage, 1000));
+
+function updatingTimeLocalStorage({ seconds }) {
+  localStorage.setItem(KEY_STORAGE, seconds);
+}
+
+player.setCurrentTime(localStorage.getItem(KEY_STORAGE) || 0);
